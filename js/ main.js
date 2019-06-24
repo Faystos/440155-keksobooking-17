171d6f152fd15220a7c8e1fa71b48f94f54538da
Faystos <fayst_@bk.ru>
2019-06-24 15:15:30 +0300

    продолжаю работу над личным проектом

--- js/ main.js
+++ js/ main.js
@@ -1,5 +1,5 @@
 'use strict';
-//var map = document.querySelector('.map');
-//var faded = document.querySelector('.map--faded');
+var map = document.querySelector('.map');
+var faded = document.querySelector('.map--faded');
 
-//faded.classList.remove('.map--faded');
+faded.classList.remove('map--faded');
