3d8ee7a623a4736603f23e104b812879c5c6f2af
Faystos <fayst_@bk.ru>
2019-06-29 02:40:51 +0300

    закончил задание: Личный проект: пока все дома; жду ревью

--- js/main.js
+++ js/main.js
@@ -1,11 +1,63 @@
 'use strict';
 var faded = document.querySelector('.map--faded');
+var mapWight = document.querySelector('.map').offsetWidth;
+var pins = document.querySelector('.map__pins');
+faded.classList.remove('map--faded');
+var DIMENSIONS_IMG = 40;
 
-var listResidentialObjects = {
-  'author': {
-    'avatar': 'img/avatars/user{{01}}.png'
-  }
+var listResidentialObjects = [];
+var listTypes = [
+  'palace',
+  'flat',
+  'house',
+  'bungalo'
+];
 
+var getRandomInt = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-faded.classList.remove('map--faded');
+for (var i = 0; i <= 7; i++) {
+  var prefixZeroImg = i < 9 ? 0 : '';
+  var imgList = 'img/avatars/user' + prefixZeroImg + (i + 1) + '.png';
+  var listType = listTypes[getRandomInt(0, 3)];
+  var mapLocationX = getRandomInt(0 + DIMENSIONS_IMG, mapWight - DIMENSIONS_IMG);
+  var mapLocationY = getRandomInt(130, 630);
+
+  var author = {
+    'avatar': imgList
+  };
+
+  var offer = {
+    'type': listType
+  };
+
+  var locationMap = {
+    'x': mapLocationX,
+    'y': mapLocationY
+  };
+
+  var listResidentialObject = {
+    'author': author,
+    'offer': offer,
+    'locationMap': locationMap
+  };
+
+  listResidentialObjects.push(listResidentialObject);
+}
+
+var fragObjPin = document.createDocumentFragment();
+for (i = 0; i <= 7; i++) {
+  var pin = document.createElement('button');
+  var pinImg = document.createElement('img');
+  pin.className = 'map__pin';
+  pin.style.left = listResidentialObjects[i].locationMap.x + 'px';
+  pin.style.top = listResidentialObjects[i].locationMap.y + 'px';
+  pinImg.src = listResidentialObjects[i].author.avatar;
+  pinImg.width = DIMENSIONS_IMG;
+  pinImg.height = DIMENSIONS_IMG;
+  pin.appendChild(pinImg);
+  fragObjPin.appendChild(pin);
+}
+
+pins.appendChild(fragObjPin);
