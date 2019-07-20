152a75ef713531b59820fb37b47df869065d6ffe
Vladimir <fayst_@bk.ru>
2019-07-20 16:37:43 +0300

    поправил от мусора

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 'use strict';
 
-(function(){
+(function () {
 
   var onButtonMainClick = function () {
     window.data.fadedMap.classList.remove('map--faded');
@@ -10,21 +10,14 @@
   window.data.buttonMain.addEventListener('click', onButtonMainClick);
 })();
 
-// ********************************************************************
+(function () {
 
-
-
-// *****************************************************************************
-
-(function(){
-
-   window.getCoords = function () {
+  window.getCoords = function () {
 
     var posX = Math.round(window.data.buttonMain.offsetTop + (window.data.buttonMain.offsetHeight / 2));
     var posY = Math.round(window.data.buttonMain.offsetLeft + (window.data.buttonMain.offsetWidth / 2));
 
-     window.data.adressInp.value = posX + ',' + posY;
+    window.data.adressInp.value = posX + ',' + posY;
   };
-    window.getCoords(window.data.buttonMain);
-
+  window.getCoords(window.data.buttonMain);
 })();
--- js/pin.js
+++ js/pin.js
@@ -1,6 +1,6 @@
+'use strict';
 
-
-(function(){
+(function () {
 
   var onMouseDown = function (evt) {
     evt.preventDefault();
