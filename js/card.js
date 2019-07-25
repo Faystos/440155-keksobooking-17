8cdabf8b1fc894d16928528a1f900404aa52ca69
Vladimir <fayst_@bk.ru>
2019-07-25 20:00:27 +0300

    сделал генерацию карточек по данным из выгружен объекта

--- js/card.js
+++ js/card.js
@@ -2,71 +2,22 @@
 
 (function () {
 
-  // var listResidentialObjects = [];
-  // var listTypes = [
-  //   'palace',
-  //   'flat',
-  //   'house',
-  //   'bungalo'
-  // ];
-
   var getRandomInt = function (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
-  // for (var i = 0; i <= 7; i++) {
-  //   var prefixZeroImg = i < 9 ? 0 : '';
-  //   var imgList = 'img/avatars/user' + prefixZeroImg + (i + 1) + '.png';
-  //   var listType = listTypes[getRandomInt(0, 3)];
-  //   var mapLocationX = getRandomInt(0 + window.data.DIMENSIONS_IMG / 2, window.data.mapWight - window.data.DIMENSIONS_IMG / 2);
-  //   var mapLocationY = getRandomInt(130, 630);
-  //
-  //   var author = {
-  //     'avatar': imgList
-  //   };
-  //
-  //   var offer = {
-  //     'type': listType
-  //   };
-  //
-  //   var locationMap = {
-  //     'x': mapLocationX,
-  //     'y': mapLocationY
-  //   };
-  //
-  //   var listResidentialObject = {
-  //     'author': author,
-  //     'offer': offer,
-  //     'locationMap': locationMap
-  //   };
-  //
-  //   listResidentialObjects.push(listResidentialObject);
-  // }
-
-  // for (i = 0; i <= 7; i++) {
-  //   var pin = document.createElement('button');
-  //   var pinImg = document.createElement('img');
-  //   pin.className = 'map__pin';
-  //   pin.style.left = listResidentialObjects[i].locationMap.x + 'px';
-  //   pin.style.top = listResidentialObjects[i].locationMap.y + 'px';
-  //   pinImg.src = listResidentialObjects[i].author.avatar;
-  //   pinImg.width = window.data.DIMENSIONS_IMG;
-  //   pinImg.height = window.data.DIMENSIONS_IMG;
-  //   pinImg.alt = 'заголовок объявления';
-  //   pin.appendChild(pinImg);
-  //   window.data.fragObjPin.appendChild(pin);
-  // }
-
-
-  // window.load(function (data) {
-  //
-  // });
-
   var onSuccess = function (data) {
-    console.log(data);
-    //  var pin = document.createElement('button');
-    for (var i = 0; i <= 7; i++) {
+    for (var i = 0; i < data.length; i++) {
       var pin = document.createElement('button');
+      var pinImg = document.createElement('img');
+      pin.className = 'map__pin';
+      pinImg.src = data[i].author.avatar;
+      pinImg.width = window.data.DIMENSIONS_IMG;
+      pinImg.height = window.data.DIMENSIONS_IMG;
+      pinImg.alt = data[i].offer.title;
+      pin.style.left = data[i].location.x + 'px';
+      pin.style.top = data[i].location.y + 'px';
+      pin.appendChild(pinImg);
       window.data.fragObjPin.appendChild(pin);
     }
   };
